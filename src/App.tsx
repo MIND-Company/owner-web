228549b4ebe16cdacc95533a8151bb114062b6b6
Dmitry <dimakorobitcyn@yandex.ru>
2022-09-30 20:31:15 +0500

    Add ESLint

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import { LoginPage } from "./pages/login-page/login-page";
+import {BrowserRouter, Route, Routes, Navigate} from 'react-router-dom';
+import {LoginPage} from './pages/login-page/login-page';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Navigate to='/login' />} />
+				<Route path='/login' element={<LoginPage />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- src/pages/login-page/login-page.tsx
+++ src/pages/login-page/login-page.tsx
@@ -1,90 +1,89 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import styles from "./login-page.module.css";
-import { encodingToB64 } from "../../utils/encodingToB64";
-import { path } from "../../utils/path";
+import React, {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+import styles from './login-page.module.css';
+import {encodingToB64} from '../../utils/encodingToB64';
+import {path} from '../../utils/path';
 
 export const LoginPage = () => {
-  const navigate = useNavigate();
-  const [currentLogin, setCurrentLogin] = useState("");
-  const [currentPassword, setCurrentPassword] = useState("");
-  const [isError, setError] = useState(false);
+	const navigate = useNavigate();
+	const [currentLogin, setCurrentLogin] = useState('');
+	const [currentPassword, setCurrentPassword] = useState('');
+	const [isError, setError] = useState(false);
 
-  const checkResponse = function () {
-    async function getResponse() {
-      const response = await fetch(
-        `${path}`,
-        {
-          headers: {
-            Authorization:
-              "Auth " + encodingToB64(currentLogin + ":" + currentPassword),
-          },
-        }
-      );
+	const checkResponse = function () {
+		async function getResponse() {
+			const response = await fetch(
+				`${path}`,
+				{
+					headers: {
+						Authorization:
+              'Auth ' + encodingToB64(currentLogin + ':' + currentPassword),
+					},
+				},
+			);
 
+			// Моки
+			const login = 'login';
+			const password = '123';
+			// Моки
 
-// моки 
- const login = "login";
- const password = "123";
- // моки
+			if (currentLogin === login && currentPassword === password) {
+				navigate('/parkings');
+				localStorage.setItem(
+					'token',
+					encodingToB64(currentLogin + ':' + currentPassword),
+				);
+			} else {
+				setError(true);
+			}
+		}
 
-   if (currentLogin === login && currentPassword === password) {
-     navigate("/parkings",);
-     localStorage.setItem(
-       "token",
-       encodingToB64(currentLogin + ":" + currentPassword)
-     );
-   } else {
-     setError(true);
-   }
- }
+		getResponse();
+	};
 
- getResponse();
-};
+	//   Const data = await response.json();
 
-//   const data = await response.json();
+	//       if (response.ok) {
+	//         navigate("/parkings", { state: data });
+	//         localStorage.setItem(
+	//           "token",
+	//           encodingToB64(currentLogin + ":" + currentPassword)
+	//         );
+	//       } else {
+	//         setError(true);
+	//       }
+	//     }
 
-//       if (response.ok) {
-//         navigate("/parkings", { state: data });
-//         localStorage.setItem(
-//           "token",
-//           encodingToB64(currentLogin + ":" + currentPassword)
-//         );
-//       } else {
-//         setError(true);
-//       }
-//     }
+	//     getResponse();
+	//   };
 
-//     getResponse();
-//   };
+	const handleChangeLogin = (event: {target: {value: React.SetStateAction<string>}}) => {
+		setCurrentLogin(event.target.value);
+	};
 
-  const handleChangeLogin = (event: { target: { value: React.SetStateAction<string>; }; }) => {
-    setCurrentLogin(event.target.value);
-  };
+	const handleChangePassword = (event: {target: {value: React.SetStateAction<string>}}) => {
+		setCurrentPassword(event.target.value);
+	};
 
-  const handleChangePassword = (event: { target: { value: React.SetStateAction<string>; }; }) => {
-    setCurrentPassword(event.target.value);
-  };
-
-  return (
-    <div>
-      <input
-        placeholder="Логин"
-        value={currentLogin}
-        onChange={handleChangeLogin}
-      />
-      <input
-        placeholder="Пароль"
-        value={currentPassword}
-        onChange={handleChangePassword}
-        type="password"
-      />
-      <button className={styles.checkResponse} onClick={checkResponse}>
+	return (
+		<div>
+			<input
+				placeholder='Логин'
+				value={currentLogin}
+				onChange={handleChangeLogin}
+			/>
+			<input
+				placeholder='Пароль'
+				value={currentPassword}
+				onChange={handleChangePassword}
+				type='password'
+			/>
+			<button className={styles.checkResponse} onClick={checkResponse}>
         Войти
-      </button>
-      {isError && (
-        <div className={styles.error}>Неправильный логин или пароль!</div>
-      )}
-    </div>
-  );
+			</button>
+			{isError && (
+				<div className={styles.error}>Неправильный логин или пароль!</div>
+			)}
+		</div>
+	);
 };
