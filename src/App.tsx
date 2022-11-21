edbf4e6280b9e58e83e1209f0e72df0df0a6ce72
Dmitry <dimakorobitcyn@yandex.ru>
2022-11-21 17:11:01 +0500

    create parkings info

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter, Route, Routes, Navigate} from 'react-router-dom';
 import {LoginPage} from './pages/login-page/login-page';
+import {ParkingsPage} from './pages/parkings-page/parkings-page';
 
 function App() {
 	return (
@@ -9,6 +10,7 @@ function App() {
 			<Routes>
 				<Route path='/' element={<Navigate to='/login' />} />
 				<Route path='/login' element={<LoginPage />} />
+				<Route path='/parkings' element={<ParkingsPage />} />
 			</Routes>
 		</BrowserRouter>
 	);
