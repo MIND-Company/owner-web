002fab401ec494e32c82007aa4a023023dfbaa2c
i.ermolaev <Ermolaev.ID@yandex.ru>
2023-01-15 00:31:39 +0500

    update

--- src/children/cabinet/children/parkings/children/parking/children/transport/Transport.container.tsx
+++ src/children/cabinet/children/parkings/children/parking/children/transport/Transport.container.tsx
@@ -8,6 +8,7 @@ import { useParking } from '../../hooks/parking.hook';
 import { useParams } from 'react-router-dom';
 import { Button, useUpgradedState } from '@ermolaev/mind-ui';
 import classes from './Transport.styles.module.css';
+import { useTimeFormatter } from '../../../../../../../../hooks/time-formatter.hook';
 
 export const TransportContainer = () => {
   const parkingApi = useParking();
@@ -44,7 +45,7 @@ export const TransportContainer = () => {
           return (
             <div className={classes.item} key={index}>
               <div>Номер ТС: {el.plate}</div>
-              <div>Время въезда: {new Date(el.entryTime).toLocaleString()}</div>
+              <div>Время въезда: {useTimeFormatter(el.entryTime)}</div>
             </div>
           );
         })}
@@ -57,12 +58,8 @@ export const TransportContainer = () => {
             return (
               <div className={classes.item} key={index}>
                 <div>Номер ТС: {el.plate}</div>
-                <div>
-                  Время въезда: {new Date(el.entryTime).toLocaleString()}
-                </div>
-                <div>
-                  Время выезда: {new Date(el.checkoutTime).toLocaleString()}
-                </div>
+                <div>Время въезда: {useTimeFormatter(el.entryTime)}</div>
+                <div>Время выезда: {useTimeFormatter(el.entryTime)}</div>
                 <div>Сумма: {el.calculatedPrice}₽</div>
               </div>
             );
---
+++ src/hooks/time-formatter.hook.ts
@@ -0,0 +1,9 @@
+export const useTimeFormatter = (time: string | Date): string => {
+  return new Date(time).toLocaleString('ru-RU', {
+    day: '2-digit',
+    month: 'long',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+};
